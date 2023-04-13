f6db76b351e8d2b239bc968d571d3310b7aaab67
Elimane Fofana <elimane.fofana@supinfo.com>
2023-04-13 18:24:38 +0200

    producer configs

--- library-events-producer/src/main/java/com/kafka/libraryeventsproducer/config/AutoCreateConfig.java
+++ library-events-producer/src/main/java/com/kafka/libraryeventsproducer/config/AutoCreateConfig.java
@@ -1,10 +1,15 @@
 package com.kafka.libraryeventsproducer.config;
 
 import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.kafka.config.TopicBuilder;
+
+import java.util.List;
 
 /**
  * @PROJECT library-events-producer
@@ -13,6 +18,8 @@ import org.springframework.kafka.config.TopicBuilder;
 @Configuration
 @Profile("local")
 public class AutoCreateConfig {
+
+
 
   @Bean
   public NewTopic libraryEvents(){
--- library-events-producer/src/main/java/com/kafka/libraryeventsproducer/config/ConfigProperties.java
+++ library-events-producer/src/main/java/com/kafka/libraryeventsproducer/config/ConfigProperties.java
@@ -2,7 +2,11 @@ package com.kafka.libraryeventsproducer.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import java.util.List;
 
@@ -11,7 +15,6 @@ import java.util.List;
  * @Author Elimane on 08/04/2023
  */
 @Configuration
-@ConfigurationProperties(prefix = "cluster")
 public class ConfigProperties {
 
   private List<String> topics;
